083899130a5613f9861bbd3febc580520f9fcb90
dashdots <faculacola@gmail.com>
2016-04-19 21:09:10 +0800

    remove object.assign

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const assign = require('object.assign/polyfill')();
+import { assign } from 'lodash';
+
+if (!has(Object, 'assign')) {
+  Object.assign = assign;
+}
 import * as loader from './src/loader';
 import * as storage from './src/storage';
 export * from './src/loader';
 export const LocalStorage = storage;
-export default assign({}, loader, {LocalStorage: storage});
+export default Object.assign({}, loader, {LocalStorage: storage});
